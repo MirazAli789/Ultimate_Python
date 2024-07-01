ff9fefd6c48b0b1c36db888ac92f0296a61d9089
Miraz <2609miraj@gmail.com>
2024-07-01 12:33:52 +0530

    wdq

--- Chapter 3/01_intro_to_strings.py
+++ Chapter 3/01_intro_to_strings.py
@@ -3,5 +3,6 @@ name = "Harry"
 
 nameshort = name[0:3] # start from index 0 all the way till 3 (excluding 3)
 print(nameshort)
-character1 = name[1]
+character1 = name[0]
 print(character1)
+character2 = name[1]
