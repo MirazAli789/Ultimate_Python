e42006eec52365b4fcf4a199c7afd82147786d03
Miraz <2609miraj@gmail.com>
2024-07-11 11:25:15 +0530

    k

--- Chapter 5/test1.py
+++ Chapter 5/test1.py
@@ -12,4 +12,17 @@
 # print(s)
 # print(s.pop())
 # print(s)
+s1 = {1, 4, 5}
+s2 = {2, 3, 3, 4, 4, 5, 5, 6}
+print(s1.intersection(s2))
+print(s1.union(s2))
+print(s1 - s2)
+sub = {4, 5}
+print(sub.issubset(s1))
+print(sub.issubset(s2))
+print(s1.isdisjoint(s2))
 
+s3 = {1, 2}
+s4 = {3, 4}
+print(s3.intersection(s4))
+print(s3.isdisjoint(s4))
