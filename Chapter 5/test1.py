04d5936f0215615106f63cd9c5bad4dcbe0b7604
Miraz <2609miraj@gmail.com>
2024-07-11 11:07:35 +0530

    Refactor set initialization and remove redundant code

--- Chapter 5/test1.py
+++ Chapter 5/test1.py
@@ -1,3 +1,15 @@
 # sets are unordered mutable collection of unique elements
-s = {}
-print(type(s))
+# s = {} # an empty dicitonary
+# print(type(s))
+# st = set() # empty set
+# print(type(st))
+# s = {2, 3, 4, 2, "harry", None, True, 3, 4 + 6j}
+# print(s, type(s))
+# print(s)
+# s.add(45)
+# print(s)
+# s.remove(45)
+# print(s)
+# print(s.pop())
+# print(s)
+
