519101cc465992c81e2811974759e6d16cac59f5
Miraz <2609miraj@gmail.com>
2024-07-18 00:56:32 +0530

    Refactor for loop to use consistent spacing in range function

--- Chapter 7/04_for_loops.py
+++ Chapter 7/04_for_loops.py
@@ -4,7 +4,7 @@ for i in range(4):
 # step size in range() function
 print()
 print()
-for i in range(0,11,2):
+for i in range(0, 11, 2):
     print(i)
 # output 0 2 4 6 8 10
 # range(start_index,end_index,step_size)
